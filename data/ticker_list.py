0aeaad9634384410911729889094deaf93a7c0b1
Ayushi828 <aayushi.aa613@gmail.com>
2026-06-21 00:27:59 +0530

    ticker list

---
+++ data/ticker_list.py
@@ -0,0 +1,12 @@
+
+tickers = [ "HDFCBANK.NS",
+            "FEDERALBNK.NS", 
+            "SBIN.NS",
+            "BANKBARODA.NS",
+            "CHOLAFIN.NS",
+            "SUNDARMFIN.NS",
+            "CRISIL.NS",
+            "JMFINANCIL.NS",
+            "CANFINHOME.NS",
+            "BAJAJFINSV.NS"
+          ]

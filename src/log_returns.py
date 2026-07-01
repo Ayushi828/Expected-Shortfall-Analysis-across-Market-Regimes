9c29c6cd2e5aeb75664ded4b7b5f3507c039c065
Ayushi828 <aayushi.aa613@gmail.com>
2026-07-01 15:27:10 +0530

    ..

---
+++ src/log_returns.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def cal_returns(adj_close_price):
+    returns = np.log(adj_close_price/adj_close_price.shift(1))
+    returns = returns.dropna()
+    print(returns)
+
+    return returns

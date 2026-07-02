147f50cdaace8bd42cec22cd45095a25f82ceded
Ayushi828 <aayushi.aa613@gmail.com>
2026-07-02 23:17:43 +0530

    ...
    Add setup script for package installation

---
+++ run_setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name = "Tail-Risk-and-Regime-Shift-Analysis-in-Financial-Portfolios", version = " 0.1", packages = find_packages())
+
+
+
+
+# Run this file in terminal by typing this :   python run.py install

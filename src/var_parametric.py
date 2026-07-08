0175ea4029f780db0d3365c31921bad4a7065e4d
Ayushi828 <aayushi.aa613@gmail.com>
2026-07-08 23:23:14 +0530

    ..

--- src/var_parametric.py
+++ src/var_parametric.py
@@ -3,14 +3,14 @@ from scipy.stats import norm
 
 
 def covar_stddev(returns, w):
-    cov_mat = returns.cov() * 250 
+    cov_mat = returns.cov() * 248
     std_dev = np.sqrt(w.T @ cov_mat @ w)
 
     return cov_mat, std_dev
 
 def cal_var_p(value, std_dev, cl, d):
     VAR_p= []
-    var_p =  value * std_dev * norm.ppf(cl) *np.sqrt(d/252)
+    var_p =  value * std_dev * norm.ppf(cl) *np.sqrt(d/248)
     VAR_p.append(var_p)
     
     return VAR_p

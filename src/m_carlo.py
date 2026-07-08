15b7da93c15643f268d00c0c714e29ff82b4494c
Ayushi828 <aayushi.aa613@gmail.com>
2026-07-08 23:24:24 +0530

    ..

--- src/m_carlo.py
+++ src/m_carlo.py
@@ -1,23 +1,19 @@
 import numpy as np
 
-def cal_var_ms(returns, w, value, c, d, s):
-    Var_ms = []
-
-    cov_mat = returns.cov() * 250
-    mean_returns = returns.mean()
-    c_matrix = returns.cov() * 250
-
-    value_mean = np.dot(w, returns)* 250
-    std_dev = np.sqrt(w.T @ cov_mat @ w)
-
-    nday_mean = value * (d/ 250)
-    nday_std = std_dev * np.sqrt(d/250)
-
-    sim_returns = np.random.normal(nday_mean, nday_std, s)
-
-    sim_pnl = value * sim_returns
-
-    var_s = -np.percentile(sim_pnl, 100*(1-c))
-    Var_ms.append(var_s)
-    print("VaR calculated through monte carlo is", Var_ms)
-    return Var_ms, sim_pnl
+def cal_var_mcs(returns, wt , value, c, d, s):
+    
+    mean_returns = returns.mean()          
+    cov_mat = returns.cov() * 248       
+        
+    portf_mean = np.dot(wt, mean_returns) * 248
+    std_dev = np.sqrt(wt.T @ cov_mat @ wt)
+        
+    x_mean = portf_mean * (d / 248)
+    x_stdev  = std_dev  * np.sqrt(d / 248)
+        
+    sim_returns = np.random.normal(x_mean, x_stdev, s)
+    simulated_pnl = value * sim_returns    
+    
+    VAR_MS = -np.percentile(simulated_pnl, 100 * (1 - c))
+    
+    return VAR_MS, simulated_pnl                                

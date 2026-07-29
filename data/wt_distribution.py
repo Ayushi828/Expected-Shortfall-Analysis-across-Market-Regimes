15cc9537a9a11b6367826b3d79ce8344dea3a08e
Ayushi828 <aayushi.aa613@gmail.com>
2026-07-30 00:06:56 +0530

    ..

--- data/wt_distribution.py
+++ data/wt_distribution.py
@@ -1,18 +1,19 @@
 import numpy as np
-weigh = []
+
 
 def wt_distribution(tickers):
     print("\n",'-'*10, "\tFor weight of portfolio", '-'*10)
 
     print("\n a) Equally distributed \n b) Insert weight of each stock \n")
     ch = str(input("choose the option for weight distribution: "))
+    weigh = []
 
     if ch == 'a':
-        weight = np.array([1/len(tickers)] * len(tickers))
+        weigh = np.array([1/len(tickers)] * len(tickers))
 
     else: 
-        for w in range(1,11):
-            w = float(input(f"weight for stock{w} : \t" ))
+        for w in range(len(tickers)):
+            w = float(input(f"weight for stock{w+1} : \t" ))
             weigh.append(w)
 
     return weigh
